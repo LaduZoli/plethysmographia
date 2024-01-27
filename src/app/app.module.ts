4e13947053fe361da72ba56b0273c5c73ee50948
Laduver Zoltan <laduzoli97@gmail.com>
2024-01-27 10:44:44 +0100

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CoreComponent } from './core/core.component';
+import { HomeComponent } from './core/home/home.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { LoginComponent } from './core/login/login.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    PageNotFoundComponent,
+    CoreComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+
+  email: string = '';
+  password: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    // Ide írd meg a bejelentkezési logikát (például HTTP hívás stb.)
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
+
+}
